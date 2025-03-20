1f9194f6236a095790c39c5f1b0ee3fb104789b2
HamzaAliKundi <hamza.alee83@gmail.com>
2025-03-21 02:43:24 +0500

    structure changing

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Banner from '../../components/Home/Banner';
 import WhoWeAre from '../../components/Home/WhoWeAre';
 import Singers from '../../components/Home/Singers';
-import Advertisment from '../../components/Home/Advertisment';
+import Advertisment from '../Advertisment';
 
 const Home = () => {
   return (
