59eb46c6e02f141525d3b068121f090bfdf66891
HamzaAliKundi <hamza.alee83@gmail.com>
2025-03-21 19:42:48 +0500

    add event listing section

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,11 +3,13 @@ import Banner from '../../components/Home/Banner';
 import WhoWeAre from '../../components/Home/WhoWeAre';
 import Singers from '../../components/Home/Singers';
 import Advertisment from '../Advertisment';
+import EventListing from '../../components/Home/EventListing';
 
 const Home = () => {
   return (
     <div>
       <Banner />
+      <EventListing />
       <Advertisment />
       <Singers />
       <WhoWeAre />
