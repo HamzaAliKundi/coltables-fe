31c0dffc7242319debb39154f701a3aa30aee640
Muhammad Ahsan <itsmeahsan98@gmail.com>
2025-04-25 00:40:04 +0500

    changes

--- src/store.jsx
+++ src/store.jsx
@@ -1,10 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { venuesApi } from "./apis/venues";
+import { performersApi } from "./apis/performers";
 
 export const store = configureStore({
   reducer: {
     [venuesApi.reducerPath]: venuesApi.reducer,
+    [performersApi.reducerPath]: performersApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(venuesApi.middleware),
+    getDefaultMiddleware()
+      .concat(venuesApi.middleware)
+      .concat(performersApi.middleware),
 });
