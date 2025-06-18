134a81a2b76ba58ed5890826bdfd72d7d2f80f27
HamzaAliKundi <hamza.alee83@gmail.com>
2025-06-18 23:12:08 +0500

    remove today button

--- src/components/calendar/calendar.jsx
+++ src/components/calendar/calendar.jsx
@@ -212,7 +212,6 @@ const Calendar = () => {
         {/* Left section - Navigation */}
         <div className="rbc-btn-group w-full md:w-auto flex justify-start mb-4 md:mb-0">
           <button type="button" onClick={goToBack} className="px-4 py-2">Back</button>
-          <button type="button" onClick={goToCurrent} className="px-4 py-2">Today</button>
           <button type="button" onClick={goToNext} className="px-4 py-2">Next</button>
         </div>
 
