426498321faaff1b9257c65cbd3ce5dd72d4b6dc
HamzaAliKundi <hamza.alee83@gmail.com>
2025-06-20 21:01:01 +0500

    fix font size

--- src/components/PerformerProfile/PerformerProfile.jsx
+++ src/components/PerformerProfile/PerformerProfile.jsx
@@ -466,7 +466,7 @@ const PerformerProfile = () => {
                     </div>
                   </div>
                   <div className="absolute left-20 top-10 right-0 text-center">
-                    <h2 className="text-[#FF00A2] text-[14px] sm:text-[20px] font-space-grotesk">
+                    <h2 className="text-[#FF00A2] text-[18px] sm:text-[22px] font-space-grotesk">
                       Drag Anniversary: {" "}
                       <span className="font-medium">
                         {(() => {
